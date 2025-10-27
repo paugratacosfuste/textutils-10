fb441d7c170b48927fbd401549ae2001d2d7ad29
Jan Sternberg <155014279+Jan2134@users.noreply.github.com>
2025-10-27 13:15:36 +0100

    Update __init__.py
    task distribution

--- src/textutils/__init__.py
+++ src/textutils/__init__.py
@@ -3,22 +3,22 @@ from .core import (
     top_n,
     normalize_whitespace,
     remove_punctuation,
-    #is_palindrome,
-    #unique_words,
-    #reverse_words,
-    #capitalize_sentences,
-    #word_lengths,
-    #strip_accents,
-    #slugify,
-    #count_vowels,
-    #camel_to_snake,
-    #truncate,
-    #collapse_duplicates,
-    #is_anagram,
-    #compare_texts,
-    #replace_numbers,
-    #sentence_count,
-    #average_word_length,
+    #is_palindrome, #A
+    #unique_words, #A
+    #reverse_words, #A
+    #capitalize_sentences,#A
+    #word_lengths, #L
+    #strip_accents, #L
+    #slugify, #L
+    #count_vowels, #L
+    #camel_to_snake,#S
+    #truncate, #S
+    #collapse_duplicates, #S
+    #is_anagram, #S
+    #compare_texts, #s
+    #replace_numbers, #P
+    #sentence_count, #p
+    #average_word_length, #P
 )
 
 # Define __all__ for clarity
